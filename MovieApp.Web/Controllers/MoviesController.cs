174ab8a7c0dc23f12f07452e65f15b8f4616fcec
muratilgun <muratilgun34@gmail.com>
2021-09-04 09:32:14 +0300

    Addd created method

--- MovieApp.Web/Controllers/MoviesController.cs
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -45,6 +45,10 @@ namespace MovieApp.Web.Controllers
             return View(MovieRepository.GetById(id));
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
 
     }
 }
