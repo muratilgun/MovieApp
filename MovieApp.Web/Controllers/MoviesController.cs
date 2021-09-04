ac2c7637bdc2d0966e230c55bbd5b0ac8d212c5a
muratilgun <muratilgun34@gmail.com>
2021-09-04 12:06:03 +0300

    Updated some methods

--- MovieApp.Web/Controllers/MoviesController.cs
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -49,16 +49,9 @@ namespace MovieApp.Web.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Create(string Title, string Description, string Director, string ImageUrl, int GenreId)
+        public IActionResult Create(Movie m)
         {
-            var m = new Movie()
-            {
-                Title = Title,
-                Description = Description,
-                Director = Director,
-                ImageUrl = ImageUrl,
-                GenreId = GenreId
-            };
+
             MovieRepository.Add(m);
             return RedirectToAction("List");
         }
