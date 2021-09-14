e94ffb692714517f61afc141daa864efee510f14
muratilgun <muratilgun34@gmail.com>
2021-09-14 18:29:34 +0300

    updated movie entity

--- MovieApp.Web/Entity/Movie.cs
+++ MovieApp.Web/Entity/Movie.cs
@@ -4,11 +4,13 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MovieApp.Web.Entity
 {
     public class Movie
     {
+        //[Key,DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int MovieId { get; set; }
         [Required]
         public string Title { get; set; }
@@ -16,6 +18,7 @@ namespace MovieApp.Web.Entity
         public string Description { get; set; }
         public string ImageUrl { get; set; }
         [Required]
+        public Genre Genre { get; set; }
         public int? GenreId { get; set; }
     }
 }
