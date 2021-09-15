dc716026e3a4cdb524c12750abe686273d375b02
muratilgun <muratilgun34@gmail.com>
2021-09-16 00:49:25 +0300

    implemented  choose multiple movie genres

--- MovieApp.Web/Migrations/20210915214241_ManyToMany2.Designer.cs
+++ MovieApp.Web/Migrations/20210915214241_ManyToMany2.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieApp.Web.Data;
 
 namespace MovieApp.Web.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20210915214241_ManyToMany2")]
+    partial class ManyToMany2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
