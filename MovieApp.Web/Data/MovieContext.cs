ab1efbd592c1b44cf7d3e9561c48dd59e319295b
muratilgun <muratilgun34@gmail.com>
2021-09-15 23:58:51 +0300

    OneToOne RelationShip

--- MovieApp.Web/Data/MovieContext.cs
+++ MovieApp.Web/Data/MovieContext.cs
@@ -15,7 +15,8 @@ namespace MovieApp.Web.Data
         }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Genre> Genres { get; set; }
-        public DbSet<Director> Directors {get; set; }
+        public DbSet<User> Users {get; set; }
+        public DbSet<Person> Person {get; set; }
 
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         //{
