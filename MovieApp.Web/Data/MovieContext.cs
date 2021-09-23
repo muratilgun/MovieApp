d14f0e40a8fce9a2975d21579d4f1facfa436451
muratilgun <muratilgun34@gmail.com>
2021-09-23 22:42:48 +0300

    Added  fluent api

--- MovieApp.Web/Data/MovieContext.cs
+++ MovieApp.Web/Data/MovieContext.cs
@@ -24,5 +24,13 @@ namespace MovieApp.Web.Data
         //{
         //    optionsBuilder.UseSqlite("Data Source=movies.db");
         //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().Property(b => b.Title).IsRequired();
+            modelBuilder.Entity<Movie>().Property(b => b.Title).HasMaxLength(500);
+            modelBuilder.Entity<Genre>().Property(b => b.Name).IsRequired();
+            modelBuilder.Entity<Genre>().Property(b => b.Name).HasMaxLength(50);
+        }
     }
 }
---
+++ MovieApp.Web/Migrations/20210923194023_UpdateFluentApi.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieApp.Web.Migrations
+{
+    public partial class UpdateFluentApi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movies",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genres",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genres",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
