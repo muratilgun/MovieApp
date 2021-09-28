45be8e2b6d27fc5087c80f81801517bb8d774da5
muratilgun <muratilgun34@gmail.com>
2021-09-28 22:12:50 +0300

    Northwind databasefirst

---
+++ LINQSamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
