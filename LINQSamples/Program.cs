6ef51fbca2d6e20439bf77bfd223a6af37f95042
muratilgun <muratilgun34@gmail.com>
2021-09-29 00:19:58 +0300

    Update Program.cs

--- LINQSamples/Program.cs
+++ LINQSamples/Program.cs
@@ -16,6 +16,7 @@ namespace LINQSamples
         {
             using (var db = new NorthwindContext())
             {
+                #region ESKİ
                 //var products = db.Products.ToList();
                 //var products = db.Products.Select(p => p.ProductName).ToList();
                 //var products = db.Products.Select(p => new { p.ProductName,p.UnitPrice }).ToList();
@@ -29,8 +30,31 @@ namespace LINQSamples
                 //    Console.WriteLine(p.Name + " " +p.Price);
                 //}
                 //var product = db.Products.First();
-                var product = db.Products.Select(p=> new { p.ProductName, p.UnitPrice}).FirstOrDefault();
-                Console.WriteLine(product.ProductName + ' ' + product.UnitPrice);
+                //var product = db.Products.Select(p=> new { p.ProductName, p.UnitPrice}).FirstOrDefault();
+                //Console.WriteLine(product.ProductName + ' ' + product.UnitPrice); 
+                #endregion
+                #region ESKİ2
+                //var products = db.Products.Where(p => p.UnitPrice > 18).ToList();
+                //var products = db.Products
+                //    .Select(p => new { p.ProductName, p.UnitPrice })
+                //    .Where(p => p.UnitPrice > 18).ToList();
+                //var products = db.Products
+                //       .Where(p => p.UnitPrice > 18 && p.UnitPrice < 30).ToList();
+
+                //var products = db.Products.Where(p => p.CategoryId>= 1 && p.CategoryId<=5).ToList();
+                //var products = db.Products.Where(p => p.CategoryId== 1 || p.CategoryId==5).ToList();
+                //var products = db.Products.Where(p => p.CategoryId==1)
+                //    .Select(p => new { p.ProductName, p.UnitPrice})
+                //    .ToList();
+                //var products = db.Products.Where(i => i.ProductName == "Chai").ToList();
+                //var products = db.Products.Where(i => i.ProductName.Contains("Ch")).ToList();
+                //var products = db.Products.Where(i => i.ProductName.Contains("Ch")).ToList();
+                //foreach (var p in products)
+                //{
+                //    Console.WriteLine(p.ProductName + " " + p.UnitPrice);
+                //} 
+                #endregion
+
             }
             Console.ReadLine();
         }
